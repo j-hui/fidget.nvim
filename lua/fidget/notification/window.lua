j-hui/fidget.nvim

commit 2635400d77bf5d035e2de04b2b4205a1796ac16a

feat: render notifications with extmarks
Prior to this change, we were manually constructing an array of strings
which represented each line in the notifications buffer, and then
separately maintaining an array of highlights to apply to those lines.
Not only is this cumbersome and error-prone, it also incurs the cost of
a bunch of repeated string allocations.

Instead, this change sets the buffer full of empty lines, and then
renders each line as an ext_mark with some attached virt_text which
accepts a sort of highlighted-twine data structure, i.e., a list of
(text, highlight[]) tuples which are horizontally concatenated.

In other words, we let ext_marks do the heavy lifting of:

-       horizontally concatenation
-       highlighting
-       right-justification

The data structure may also open doors to caching and finer-grained
highlights, though neither of these are implemented yet.

Note that this commit collided with 2a29bfd5f8529f1e1ad3868a4f820de972189c90
so the issue will regress in this commit. I will address that later.

lua/fidget/notification/window.lua
@@ -337,6 +337,7 @@ function M.get_window(row, col, anchor, width, height)
     return nil
   end
 
+  width = width + M.options.x_padding
   width = math.min(width, editor_width)
   if M.options.max_width > 0 then
     width = math.min(width, M.options.max_width)
@@ -413,10 +414,10 @@ end
 --- Returns nil if Fidget window could not be created (e.g., because editor is
 --- too small to show Fidget).
 ---
----@param width   number
----@param height  number
+---@param height  integer
+---@param width   integer
 ---@return number|nil window_id
-function M.show(width, height)
+function M.show(height, width)
   local row, col, anchor = M.get_window_position()
   return M.get_window(row, col, anchor, width, height)
 end
@@ -424,71 +425,26 @@ end
 --- Replace the set of lines in the Fidget window, right-justify them, and apply
 --- highlights.
 ---
----@param lines       string[]                  lines to place into buffer
----@param highlights  NotificationHighlight[]   list of highlights to apply
----@param right_align boolean                   whether to right-justify the text
----@return integer max_width
-function M.set_lines(lines, highlights, right_align)
+---
+---@param lines       NotificationLine[]  lines to place into buffer
+---@param width       integer             width of longest line
+function M.set_lines(lines, width)
   local buffer_id = M.get_buffer()
   local namespace_id = M.get_namespace()
 
   -- Clear previous highlights
   vim.api.nvim_buf_clear_namespace(buffer_id, namespace_id, 0, -1)
+  vim.api.nvim_buf_set_lines(buffer_id, 0, -1, false, {})
 
-  local line_widths = {}
-  local max_width = 0
-  vim.api.nvim_buf_call(buffer_id, function()
-    for l, line in ipairs(lines) do
-      line_widths[l] = vim.fn.strdisplaywidth(line)
-      max_width = math.max(max_width, line_widths[l])
-    end
-  end)
-
-  if right_align then
-    local offsets = {}
-
-    -- Right justify each line by padding spaces
-    for l, width in ipairs(line_widths) do
-      local offset = max_width - width
-      lines[l] = string.rep(" ", offset) .. lines[l]
-      offsets[l] = offset
-    end
-
-    -- Replace entire buffer with new set of lines
-    vim.bo[buffer_id].modifiable = true
-    vim.api.nvim_buf_set_lines(buffer_id, 0, -1, false, lines)
-    vim.bo[buffer_id].modifiable = false
-    vim.bo[buffer_id].modified = false
-
-    for _, highlight in ipairs(highlights) do
-      local offset = offsets[highlight.line + 1] -- highlight.line is 0-based, so needs +1
-      vim.api.nvim_buf_add_highlight(
-        buffer_id,
-        namespace_id,
-        highlight.hl_group,
-        highlight.line,
-        highlight.col_start + offset,
-        highlight.col_end == -1 and -1 or highlight.col_end + offset)
-    end
-  else
-    -- Replace entire buffer with new set of lines
-    vim.bo[buffer_id].modifiable = true
-    vim.api.nvim_buf_set_lines(buffer_id, 0, -1, false, lines)
-    vim.bo[buffer_id].modifiable = false
-    vim.bo[buffer_id].modified = false
-
-    for _, highlight in ipairs(highlights) do
-      vim.api.nvim_buf_add_highlight(
-        buffer_id,
-        namespace_id,
-        highlight.hl_group,
-        highlight.line,
-        highlight.col_start,
-        highlight.col_end)
-    end
+  for _, line in ipairs(lines) do
+    vim.api.nvim_buf_set_lines(buffer_id, -1, -1, false, { "" })
+    local last = vim.api.nvim_buf_line_count(buffer_id) - 1
+    vim.api.nvim_buf_set_extmark(buffer_id, namespace_id, last, 0, {
+      virt_text = line,
+      virt_text_pos = "eol_right_align",
+    })
   end
-
-  return max_width
+  M.show(#lines, width)
 end
 
 --- Close the Fidget window and associated buffers.

lua/fidget/notification.lua
@@ -363,16 +363,15 @@ notification.poller = poll.Poller {
     notification.model.tick(self:now(), state)
 
     -- TODO: if not modified, don't re-render
-    local v = notification.view.render(self:now(), state.groups)
+    local lines, width = notification.view.render(self:now(), state.groups)
 
-    if #v.lines > 0 then
+    if #lines > 0 then
       if state.view_suppressed then
         return true
       end
 
       notification.window.guard(function()
-        local width = notification.window.set_lines(v.lines, v.highlights, true)
-        notification.window.show(width, #v.lines)
+        notification.window.set_lines(lines, width)
       end)
       return true
     else

lua/fidget/notification/view.lua
@@ -3,19 +3,8 @@
 --- TODO: partial/in-place rendering, to avoid building new strings.
 local M = {}
 
----@class NotificationView
----@field lines       string[]                  text to show in the notification
----@field highlights  NotificationHighlight[]   buf_add_highlight() params, applied in order
-
----@class NotificationRenderItem
----@field lines      string[]                 displayed message for the item
----@field highlights NotificationHighlight[]  buf_add_highlight() params for lines field
-
----@class NotificationHighlight
----@field hl_group    string    what highlight group to add
----@field line        number    (0-indexed) line number to add highlight
----@field col_start   number    (byte-indexed) column to start highlight
----@field col_end     number    (byte-indexed) column to end highlight
+--- A list of (text, highlight[]) tuples of highlighted tokens.
+---@class NotificationLine : {[1]: string, [2]: string[]}[]
 
 ---@options notification.view [[
 ---@protected
@@ -70,32 +59,30 @@ M.options = {
 
 require("fidget.options").declare(M, "notification.view", M.options)
 
---- Render group separator item.
----
----@return NotificationRenderItem|nil group_separator
+---@param s string|nil
+---@return integer len
+local function strlen(s)
+  if s then return vim.fn.strdisplaywidth(s) else return 0 end
+end
+
+---@return NotificationLine[]|nil lines
+---@return integer                width
 function M.render_group_separator()
   if not M.options.group_separator then
-    return nil
+    return nil, 0
   end
-
-  return {
-    lines = { M.options.group_separator or nil },
-    highlights = {
-      M.options.group_separator_hl and {
-        hl_group = M.options.group_separator_hl or nil,
-        line = 0,
-        col_start = 0,
-        col_end = -1,
-      }
-    }
-  }
+  local line = M.options.group_separator
+  ---@cast line string
+  return { { { line, { M.options.group_separator_hl } } } }, strlen(line)
+  -- TODO: cache the return value, this never changes
 end
 
 --- Render the header of a group, containing group name and icon.
 ---
 ---@param   now   number    timestamp of current render frame
----@param   group Group
----@return  NotificationRenderItem|nil group_header
+---@param   group Group     group whose header we should render
+---@return  NotificationLine[]|nil group_header
+---@return  integer                width
 function M.render_group_header(now, group)
   local group_name = group.config.name
   if type(group_name) == "function" then
@@ -107,69 +94,46 @@ function M.render_group_header(now, group)
     group_icon = group_icon(now, group.items)
   end
 
-  if group_name == nil and group_icon == nil then
-    -- No group header to render
-    return nil
-  end
+  local name_tok = group_name and {
+    group_name, { group.config.group_style or "Title" }
+  }
+  local icon_tok = group_icon and {
+    group_icon, { group.config.icon_style or group.config.group_style or "Title" },
+  }
 
-  local lines, highlights = {}, {}
-
-  if group_name and group_icon then
-    -- Both group_name and group_icon are present, lay them out next to each other
+  if name_tok and icon_tok then
+    local sep_tok = { M.options.icon_separator, {} } -- TODO: cache this
+    local width = strlen(group_name) + strlen(group_icon) + strlen(M.options.icon_separator)
     if group.config.icon_on_left then
-      table.insert(lines, string.format("%s%s%s", group_icon, M.options.icon_separator, group_name))
-      table.insert(highlights, {
-        hl_group = group.config.group_style or "Title",
-        line = 0,
-        col_start = 0,
-        col_end = -1,
-      })
-      if group.config.icon_style then
-        table.insert(highlights, {
-          hl_group = group.config.icon_style,
-          line = 0,
-          col_start = 0,
-          col_end = #group_icon,
-        })
-      end
-    else -- not icon_on_left, AKA icon on right
-      -- NOTE: this branch represents the most common case (default options)
-      table.insert(lines, string.format("%s%s%s", group_name, M.options.icon_separator, group_icon))
-      table.insert(highlights, {
-        hl_group = group.config.group_style or "Title",
-        line = 0,
-        col_start = 0,
-        col_end = -1,
-      })
-      if group.config.icon_style then
-        table.insert(highlights, {
-          hl_group = group.config.icon_style,
-          line = 0,
-          col_start = #group_name + #M.options.icon_separator,
-          col_end = -1,
-        })
-      end
+      return { { icon_tok, sep_tok, name_tok } }, width
+    else
+      return { { name_tok, sep_tok, icon_tok } }, width
     end
+  elseif name_tok then
+    return { { name_tok } }, strlen(group_name)
+  elseif icon_tok then
+    return { { icon_tok } }, strlen(group_icon)
   else
-    if group_name then
-      table.insert(lines, group_name)
-      table.insert(highlights, {
-        hl_group = group.config.group_style or "Title",
-        line = 0,
-        col_start = 0,
-        col_end = -1,
-      })
-    elseif group_icon then
-      table.insert(lines, group_icon)
-      table.insert(highlights, {
-        hl_group = group.config.icon_style or group.config.group_style or "Title",
-        line = 0,
-        col_start = 0,
-        col_end = -1,
-      })
+    -- No group header to render
+    return nil, 0
+  end
+end
+
+---@param items Item[]
+---@return Item[] deduped
+---@return table<any, integer> counts
+function M.dedup_items(items)
+  local deduped, counts = {}, {}
+  for _, item in ipairs(items) do
+    local key = item.content_key or item
+    if counts[key] then
+      counts[key] = counts[key] + 1
+    else
+      counts[key] = 1
+      table.insert(deduped, item)
     end
   end
-  return { lines = lines, highlights = highlights }
+  return deduped, counts
 end
 
 --- Render a notification item, containing message and annote.
@@ -177,124 +141,95 @@ end
 ---@param item   Item
 ---@param config Config
 ---@param count  number
----@return       NotificationRenderItem|nil render_item
+---@return NotificationLine[]|nil lines
+---@return integer                max_width
 function M.render_item(item, config, count)
   if item.hidden then
-    return nil
+    return nil, 0
   end
 
-  local lines, highlights = {}, {}
-
   local msg = M.options.render_message(item.message, count)
+
+  local sep_tok = { config.annote_separator or " ", {} }
+  local lines, width = {}, 0
   for line in vim.gsplit(msg, "\n", { plain = true, trimempty = true }) do
-    table.insert(lines, line)
+    local line_tok = { line, {} }
+    if item.annote and #lines == 0 then
+      table.insert(lines, {
+        line_tok,
+        sep_tok,
+        { item.annote, { item.style } },
+      })
+      width = math.max(width, strlen(line) + strlen(sep_tok[1]) + strlen(item.annote))
+    else
+      table.insert(lines, { line_tok })
+      width = math.max(width, strlen(line))
+    end
   end
 
   if #lines == 0 then
     -- Don't render empty messages
-    return nil
+    return nil, 0
+  else
+    return lines, width
   end
-
-  if item.annote then
-    local line1 = lines[1] -- Append annote to first line
-    local col_start = #line1
-    local sep = config.annote_separator or " "
-    line1 = string.format("%s%s%s", line1, sep, item.annote)
-    lines[1] = line1
-
-    -- Insert highlight for annote
-    table.insert(highlights, {
-      hl_group = item.style,
-      line = 0,              -- 0-indexed
-      col_start = col_start, -- byte-indexed
-      col_end = -1,
-    })
-  end
-
-  return { lines = lines, highlights = highlights }
 end
 
 --- Render notifications into lines and highlights.
 ---
 ---@param now number timestamp of current render frame
 ---@param groups Group[]
----@return NotificationView view
+---@return NotificationLine[] lines
+---@return integer width
 function M.render(now, groups)
-  ---@type NotificationRenderItem[]
-  local render_items = {}
+  ---@type NotificationLine[][]
+  local chunks = {}
+  local max_width = 0
 
   for idx, group in ipairs(groups) do
     if idx ~= 1 then
-      local separator = M.render_group_separator()
-      if separator then
-        table.insert(render_items, separator)
+      local sep, sep_width = M.render_group_separator()
+      if sep then
+        table.insert(chunks, sep)
+        max_width = math.max(max_width, sep_width)
       end
     end
 
-    local group_header = M.render_group_header(now, group)
-    if group_header then
-      table.insert(render_items, group_header)
+    local hdr, hdr_width = M.render_group_header(now, group)
+    if hdr then
+      table.insert(chunks, hdr)
+      max_width = math.max(max_width, hdr_width)
     end
 
-    local counts = {}
-    for _, item in ipairs(group.items) do
-      local content_key = item.content_key
-      if content_key ~= nil then
-        if counts[content_key] then
-          counts[content_key] = counts[content_key] + 1
-        else
-          counts[content_key] = 1
-        end
-      end
-    end
-
-    local i = 1
-    for _, item in ipairs(group.items) do
+    local items, counts = M.dedup_items(group.items)
+    for i, item in ipairs(items) do
       if group.config.render_limit and i > group.config.render_limit then
         -- Don't bother rendering the rest (though they still exist)
         break
       end
-      local content_key = item.content_key
-      if content_key == nil or counts[content_key] then
-        local count = 1
-        if content_key ~= nil then
-          count = counts[content_key]
-          counts[content_key] = nil
-        end
 
-        local render_item = M.render_item(item, group.config, count)
-        if render_item then
-          table.insert(render_items, render_item)
-          i = i + 1
-        end
+      local it, it_width = M.render_item(item, group.config, counts[item.content_key or item])
+      if it then
+        table.insert(chunks, it)
+        max_width = math.max(max_width, it_width)
       end
     end
   end
 
-  local lines, highlights = {}, {}
-
   local start, stop, step
   if M.options.stack_upwards then
-    start, stop, step = #render_items, 1, -1
+    start, stop, step = #chunks, 1, -1
   else
-    start, stop, step = 1, #render_items, 1
+    start, stop, step = 1, #chunks, 1
   end
 
+  local lines = {}
   for i = start, stop, step do
-    local item, offset = render_items[i], #lines
-    for _, line in ipairs(item.lines) do
+    for _, line in ipairs(chunks[i]) do
       table.insert(lines, line)
     end
-    for _, highlight in ipairs(item.highlights) do
-      highlight.line = highlight.line + offset
-      table.insert(highlights, highlight)
-    end
   end
-
-  return {
-    lines = lines,
-    highlights = highlights,
-  }
+  return lines, max_width
 end
 
 --- Display notification items in Neovim messages.

commit bb5a54db55e72d5f33c13613efe2cba21b00dfa7

fix: avoid empty line at the top
vim's buffers seem to always have at least one line (see the Related
issue for a discussion about potentially changing this), so we can't
create an empty buffer and then add lines one by one to it. That results
in an extra line at the top. Visible with the following config:

```lua
require'fidget'.setup {
	notification = {
		window = {
			border = "rounded",
			winblend = 0,
		},
	},
}
```

The fix is to `vim.api.nvim_buf_set_lines` all the empty lines we'll
need at once.

Related: https://github.com/neovim/neovim/issues/34272
Fixes: 2635400d77bf ("feat: render notifications with extmarks")

lua/fidget/notification/window.lua
@@ -455,13 +455,14 @@ function M.set_lines(lines, width)
 
   -- Clear previous highlights
   vim.api.nvim_buf_clear_namespace(buffer_id, namespace_id, 0, -1)
-  vim.api.nvim_buf_set_lines(buffer_id, 0, -1, false, {})
 
-  for _, line in ipairs(lines) do
-    vim.api.nvim_buf_set_lines(buffer_id, -1, -1, false, { "" })
-    local last = vim.api.nvim_buf_line_count(buffer_id) - 1
+  -- Prepare empty lines for extmarks
+  local empty_lines = vim.tbl_map(function() return "" end, lines)
+  vim.api.nvim_buf_set_lines(buffer_id, 0, -1, false, empty_lines)
+
+  for iline, line in ipairs(lines) do
     if vim.fn.has("nvim-0.11.0") then
-      vim.api.nvim_buf_set_extmark(buffer_id, namespace_id, last, 0, {
+      vim.api.nvim_buf_set_extmark(buffer_id, namespace_id, iline - 1, 0, {
         virt_text = line,
         virt_text_pos = "eol_right_align",
       })
@@ -480,7 +481,7 @@ function M.set_lines(lines, width)
       else
         padded = line
       end
-      vim.api.nvim_buf_set_extmark(buffer_id, namespace_id, last, 0, {
+      vim.api.nvim_buf_set_extmark(buffer_id, namespace_id, iline - 1, 0, {
         virt_text = padded,
         virt_text_pos = "eol",
       })